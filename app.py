30d3261feeb35a6a07ffda510fb54c199786f298
HiteshTP <155635465+HiteshTP@users.noreply.github.com>
2024-09-14 11:31:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-#!/usr/bin/python
-
-import time
 from flask import Flask
+
 app = Flask(__name__)
 
-START = time.time()
-
-def elapsed():
-    running = time.time() - START
-    minutes, seconds = divmod(running, 60)
-    hours, minutes = divmod(minutes, 60)
-    return "%d:%02d:%02d" % (hours, minutes, seconds)
-
 @app.route('/')
-def root():
-    return "Hello World (Python)! (up %s)\n" % elapsed()
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=9000)
+    app.run(host='0.0.0.0', port=5000)
